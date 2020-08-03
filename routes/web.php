82b966b09fb4affc4e17188a5379c5cd4631d629
Richard <lilarjay06@gmail.com>
2020-08-03 12:22:13 +0800

    create contact template and route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Portfolio/page/homepage');
 });
+
+Route::get('/contact', function () {
+    return view('Portfolio/page/contact');
+});
